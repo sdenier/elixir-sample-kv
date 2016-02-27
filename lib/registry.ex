140f9f3907478bf06879dd224db842c475ac8d28
Simon Denier <simon@sogilis.com>
2016-02-28 00:01:57 +0100

    Use ETS for caching buckets

--- lib/registry.ex
+++ lib/registry.ex
@@ -7,7 +7,7 @@ defmodule KV.Registry do
   Starts the registry.
   """
   def start_link(name) do
-    GenServer.start_link(__MODULE__, :ok, name: name)
+    GenServer.start_link(__MODULE__, name, name: name)
   end
 
   @doc """
@@ -15,8 +15,11 @@ defmodule KV.Registry do
 
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup(server, name) when is_atom(server) do
+    case :ets.lookup(server, name) do
+      [{^name, bucket}] -> {:ok, bucket}
+      [] -> :error
+    end
   end
 
   @doc """
@@ -35,14 +38,10 @@ defmodule KV.Registry do
 
   ## Server Callbacks
 
-  def init(:ok) do
-    names = HashDict.new
-    refs = HashDict.new
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
+    refs = %{}
     {:ok, {names, refs}}
-  end
-
-  def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, HashDict.fetch(names, name), state}
   end
 
   def handle_call(:stop, _from, state) do
@@ -50,20 +49,21 @@ defmodule KV.Registry do
   end
 
   def handle_cast({:create, name}, {names, refs}) do
-    if HashDict.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      {:ok, bucket} = KV.Bucket.Supervisor.start_bucket()
-      ref = Process.monitor(bucket)
-      refs = HashDict.put(refs, ref, name)
-      names = HashDict.put(names, name, bucket)
-      {:noreply, {names, refs}}
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:noreply, {names, refs}}
+      :error ->
+        {:ok, bucket} = KV.Bucket.Supervisor.start_bucket()
+        ref = Process.monitor(bucket)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, bucket})
+        {:noreply, {names, refs}}
     end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-    {name, refs} = HashDict.pop(refs, ref)
-    names = HashDict.delete(names, name)
+    {name, refs} = Map.pop(refs, ref)
+    :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
